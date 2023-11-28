b2d82ba77e1ae5ee547f08d0d02daa6c5bc2738c
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2023-11-28 21:59:07 +0200

    Lasts modifications

--- script/script.js
+++ script/script.js
@@ -106,64 +106,68 @@ const editIcons = document.querySelectorAll('.edit');
 
 // Iterate through each edit icon and attach a click event listener
 editIcons.forEach(editIcon => {
-  editIcon.addEventListener('click', function(event) {
-    // Access the ID of the clicked task
-    const clickedId = event.target.id;
-    const clickedIdNumber = parseInt(clickedId.match(/\d+/)[0]) ;
-    //catch the tag with the coressponding id, then if it exists, delete it
-    const taskToUpdate = document.getElementById(`task-${clickedIdNumber}`); // the div that contains the task clicked
+    editIcon.addEventListener('click', function(event) {
+        // Access the ID of the clicked task
+        const clickedId = event.target.id;
+        const clickedIdNumber = parseInt(clickedId.match(/\d+/)[0]);
+        // Catch the tag with the corresponding id, then if it exists, delete it
+        const taskToUpdate = document.getElementById(`task-${clickedIdNumber}`); // the div that contains the task clicked
 
-    // get the task object to update 
-    
-    let theTask = allTasks.find(task => task.id == clickedIdNumber)
-    console.log(theTask);
-    const updateDiv = document.createElement('div');
-    updateDiv.innerHTML=`
-        <form id = 'updateForm'>
-        <label for="updateTask"> Update Task: </label>
-        <input name = 'updateTask' type = 'text' value=${theTask.task} required ></input>
-        <label for="updateDescription"> Update Description: </label>
-        <input name = 'updateDescription' type = 'text' value=${theTask.description}></input>
-        <input name = 'updatePrioritize' type = 'checkbox' value=${theTask.prioritize}></input>
-        <label for="updatePrioritize"> Priority</label>
-        <button type= "submit" >Modify</button>
-        </form>
-    `
-    taskToUpdate.appendChild(updateDiv);
-    console.log(clickedId);
-    console.log(clickedIdNumber);
+        // Check if the update form already exists for the clicked task
+        if (taskToUpdate.querySelector('#updateForm')) {
+            // If the update form already exists, exit the function or perform any other actions
+            return; // Prevent further execution of the function
+        }
 
-    let updateForm = document.getElementById('updateForm')
-    if (updateForm){
-    updateForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
+        // Get the task object to update 
+        let theTask = allTasks.find(task => task.id == clickedIdNumber);
+        console.log(theTask);
+        const updateDiv = document.createElement('div');
+        updateDiv.innerHTML = `
+            <form id='updateForm'>
+                <label for="updateTask"> Update Task: </label>
+                <input name='updateTask' type='text' value=${theTask.task} required ></input>
+                <label for="updateDescription"> Update Description: </label>
+                <input name='updateDescription' type='text' value=${theTask.description}></input>
+                <input name='updatePrioritize' type='checkbox' value=${theTask.prioritize}></input>
+                <label for="updatePrioritize"> Priority</label>
+                <button type="submit">Modify</button>
+            </form>
+        `;
+        taskToUpdate.appendChild(updateDiv);
+        console.log(clickedId);
+        console.log(clickedIdNumber);
 
-    // create an object with the data comming from the form
-    const updatedData = {
-        task: this.elements['updateTask'].value,
-        description: this.elements['updateDescription'].value,
-        prioritize: this.elements['updatePrioritize'].checked
-    };
+        let updateForm = document.getElementById('updateForm');
+        if (updateForm) {
+            updateForm.addEventListener('submit', function(event) {
+                event.preventDefault(); // Prevent the default form submission
 
-    updateOneTask(clickedIdNumber, updatedData); // call the function to update the task
-    
-    function updateOneTask(id_task, objectTask) {
-        allTasks = allTasks.map(task => {
-            if (task.id == id_task) {
-                task.task = objectTask.task;
-                task.description = objectTask.description;
-                task.prioritize = objectTask.prioritize;
-            }
-            return task;
-        })
-        localStorage.setItem('allTasks', JSON.stringify(allTasks));
-        console.log('Task updated, redirecting to index.html');
-        location.reload();
-    
-    }
-});
-}
-  });
+                // create an object with the data coming from the form
+                const updatedData = {
+                    task: this.elements['updateTask'].value,
+                    description: this.elements['updateDescription'].value,
+                    prioritize: this.elements['updatePrioritize'].checked
+                };
+
+                updateOneTask(clickedIdNumber, updatedData); // call the function to update the task
+
+                function updateOneTask(id_task, objectTask) {
+                    allTasks = allTasks.map(task => {
+                        if (task.id == id_task) {
+                            task.task = objectTask.task;
+                            task.description = objectTask.description;
+                            task.prioritize = objectTask.prioritize;
+                        }
+                        return task;
+                    })
+                    localStorage.setItem('allTasks', JSON.stringify(allTasks));
+                    console.log('Task updated, redirecting to index.html');
+                    location.reload();
+                }
+            });
+        }
+    });
 });
 
 
